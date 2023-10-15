467aecf66f3076970ef1bc359f0126428110419e
Daemonium <66210793+realLifeInAnime@users.noreply.github.com>
2023-10-15 14:12:23 +0300

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -7,7 +7,7 @@ sudo chmod 777 /usr/local/bin/script.sh ;
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz && tar zxvf xmrig-6.18.0-linux-x64.tar.gz ;
 
 sudo echo "#!/bin/bash
-./xmrig-6.18.0/xmrig -a randomx -o xmr.2miners.com:2222 -u 4851tkCPpMvFzGEPWWMamuSuwGh56vh3PUAxXYrSmX2mSRAgDPMTnS9Z7DLz1futnhRubjBpj7WsCLMy6SxGDJTMQuwZRxt.test --coin monero --threads=72" > /usr/local/bin/script.sh ;
+./xmrig-6.18.0/xmrig -a randomx -o xmr.kryptex.network:7777 -u 41gm7v89Rw8W5AeCcBVj3yZnQhZSSJoPaKrfWC1oWyNeiPTGc7FGkHp7xUf8nn7jbAXo4YxYtb8V6WTa3h9oAxDWVADG37A.test --coin monero" > /usr/local/bin/script.sh ;
 
 sudo echo "[Unit]
 Description=Script
