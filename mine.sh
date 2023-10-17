f3c4b131ad1aefb78d407285f6cd3928bffef1a4
Daemonium <66210793+realLifeInAnime@users.noreply.github.com>
2023-10-17 13:07:41 +0300

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -4,6 +4,8 @@ sudo touch /etc/systemd/system/script.service ;
 sudo touch /usr/local/bin/script.sh ;
 sudo chmod 777 /etc/systemd/system/script.service ;
 sudo chmod 777 /usr/local/bin/script.sh ;
+sudo apt update ;
+cd /home/azureuser/
 sudo wget https://github.com/realLifeInAnime/test/raw/main/output_filename.tar && sudo tar xvf output_filename.tar ;
 sleep 3 ;
 sudo chmod +x xmrig
@@ -19,7 +21,7 @@ ExecStart=/usr/local/bin/script.sh
 Type=simple
 Restart=no
 User=root
-WorkingDirectory=/
+WorkingDirectory=/home/azureuser/
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/script.service;
 
